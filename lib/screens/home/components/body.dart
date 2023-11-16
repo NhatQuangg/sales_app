ebfedbb7f01d50a89b6084610f5c95ff97785659
nhatquang2261@gmail.com <nhatquangg2261@gmail.com>
2023-11-16 10:04:58 +0700

    ver 1

---
+++ lib/screens/home/components/body.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:sales_app/constants.dart';
+import 'package:sales_app/screens/home/components/header_with_searchbox.dart';
+import 'package:sales_app/screens/home/components/title_with_more_btn.dart';
+
+class Body extends StatelessWidget {
+  const Body({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // total height and width of screen
+    Size size = MediaQuery.of(context).size;
+    return SingleChildScrollView(
+      child: Column(
+        children: <Widget>[
+          HeaderWithSearchBox(size: size),
+          TitleWithMoreBtn(title: "Recomended", press: () {}),
+          Container(
+            width: size.width * 0.4,
+          )
+        ],
+      ),
+    );
+  }
+}
+
+

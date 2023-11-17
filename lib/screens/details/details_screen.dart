d6d6fc4bd7e72e7d1f823db0208235ca86b45c6e
nhatquang2261@gmail.com <nhatquangg2261@gmail.com>
2023-11-17 21:58:29 +0700

    ver 1.2.1

--- lib/screens/details/details_screen.dart
+++ lib/screens/details/details_screen.dart
@@ -1,39 +1,65 @@
 import 'package:flutter/material.dart';
 import 'package:sales_app/constants.dart';
-import 'package:sales_app/screens/details/components/body.dart';
 import 'package:sales_app/screens/details/components/details_description.dart';
 import 'package:sales_app/screens/details/components/details_first.dart';
 
-class DetailsScreen extends StatelessWidget {
+class DetailsScreen extends StatefulWidget {
   const DetailsScreen({super.key});
+
+  @override
+  _DetailsScreenState createState() => _DetailsScreenState();
+}
+
+class _DetailsScreenState extends State<DetailsScreen> {
+  int _currentIndex = 0;
 
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
-        length: 2,
-        child: Scaffold(
-          body: TabBarView(
-            children: [
-              // Body(),
-              DetailsFirst(),
-              DetailsDescription()
+      length: 2,
+      child: Scaffold(
+        body: TabBarView(
+          physics: NeverScrollableScrollPhysics(),
+          children: [
+            DetailsFirst(),
+            DetailsDescription(),
+          ],
+        ),
+        bottomNavigationBar: Container(
+          height: 70,
+          color: Colors.white,
+          child: TabBar(
+            labelColor: Colors.white,
+            labelStyle: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 15
+            ),
+            unselectedLabelColor: kTextColor,
+            indicator: BoxDecoration(
+              color: kPrimaryColor,
+              borderRadius: BorderRadius.only(
+                topRight: _currentIndex == 0
+                    ? Radius.circular(50) : Radius.zero,
+                topLeft: _currentIndex == 1
+                    ? Radius.circular(50) : Radius.zero,
+              )
+            ),
+            onTap: (index) {
+              setState(() {
+                _currentIndex = index;
+              });
+            },
+            tabs: [
+              Tab(
+                text: 'Buy Now',
+              ),
+              Tab(
+                text: 'Description',
+              )
             ],
           ),
-          bottomNavigationBar: Container(
-            color: Color(0xFF3F5AA6),
-            child: TabBar(
-              labelColor: Colors.white,
-              unselectedLabelColor: Colors.white70,
-              indicatorSize: TabBarIndicatorSize.tab,
-              indicatorPadding: EdgeInsets.all(5.0),
-              indicatorColor: Colors.blue,
-              tabs: [
-                Tab(icon: Icon(Icons.add)),
-                Tab(icon: Icon(Icons.add_box_outlined))
-              ],
-            ),
-          ),
         ),
+      ),
     );
   }
 }
